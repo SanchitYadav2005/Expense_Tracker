700b8f49a6511fdec6ec049467e2c353379b8e91
unknown <sanchit2005yudavanshi@gmail.com>
2023-10-10 20:37:19 +0530

    added styles to the components '

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,30 @@
-import { useState,useEffect } from "react";
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import ExpneseDetails from "../components/ExpnseDetails";
 
 function Home() {
-    const [expense, setExpense] = useState();
-    useEffect(()=>{
-        const getExpense = async ()=>{
-            const response = await axios.get('/api/expenses')
-            const json = response.json();
-            setExpense(json)
-        }
-        getExpense();
-        console.log(expense)
-    }, [])
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  );
+  const [expense, setExpense] = useState();
+  useEffect(() => {
+    const getExpense = async () => {
+      const response = await axios.get("http://localhost:4000/api/expenses");
+      const json = response.json();
+      setExpense(json);
+    };
+    getExpense();
+  }, [expense]);
+  if(expense){
+    return (
+      <>
+        <ExpneseDetails expense={expense} />
+      </>
+    );
+  }else{
+    return(
+      <>
+        <h1>Resourse not found</h1>
+      </>
+    )
+  }
 }
 
 export default Home;
