7ef279be7ac0dd2e1c889c18766b8073125920bd
unknown <sanchit2005yudavanshi@gmail.com>
2023-10-11 18:02:18 +0530

    added most of the things

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,29 +1,44 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import ExpneseDetails from "../components/ExpnseDetails";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   const [expense, setExpense] = useState();
+  // const [show, setShow] = useState(false);
   useEffect(() => {
     const getExpense = async () => {
       const response = await axios.get("http://localhost:4000/api/expenses");
-      const json = response.json();
+      const json = response.data;
       setExpense(json);
     };
     getExpense();
   }, [expense]);
-  if(expense){
+  // const handleChange = () => {
+  //   setShow(true);
+  // };
+  // if (!show) {
+  //   const btn = (
+  //     <>
+  //       <button className="btn" onChange={handleChange}>
+  //         <FontAwesomeIcon icon={faPlus} />
+  //       </button>
+  //     </>
+  //   );
+  // }
+  if (expense) {
     return (
       <>
         <ExpneseDetails expense={expense} />
       </>
     );
-  }else{
-    return(
+  } else {
+    return (
       <>
         <h1>Resourse not found</h1>
       </>
-    )
+    );
   }
 }
 
