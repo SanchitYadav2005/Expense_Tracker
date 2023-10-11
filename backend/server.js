4bc311399ee0094145624443408d3a513c7b9d2f
unknown <sanchit2005yudavanshi@gmail.com>
2023-10-11 16:40:33 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
 const port = process.env.PORT;
-const expenseRoutes = require('./routes/exprenseRoutes')
+const expenseRoutes = require('./routes/exprenseRoutes');
+const cors = require('cors');
 
+
+
+app.use(cors())
 app.use(express.json());
 
 app.get('/', (requrest,response)=>{
