258acf35b6503a9b18010a55f04661455429d626
unknown <sanchit2005yudavanshi@gmail.com>
2023-10-14 17:12:05 +0530

    form is done

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -5,43 +5,54 @@ function Form() {
   const [title, setTitle] = useState("");
   const [spend, setSpend] = useState("");
   const [borrow, setBorrow] = useState("");
-  const handleSubmit = async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const expense = {title,spend,borrow};
+    const expense = { title, spend, borrow };
     const response = await axios.post("http://localhost:4000/api/expenses/", {
-        body: JSON.stringify(expense),
-        headers:{
-            'Content-type': 'application/json'
-        }
-    })
+      body: JSON.stringify(expense),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
     const json = await response.json();
-    if(json){
-        console.log("new entry successfully added!!");
-    }else{
-        console.log("error")
+    if (json) {
+      console.log("new entry successfully added!!");
+    } else {
+      console.log("error");
     }
-  }
+  };
   return (
     <form className="create" onSubmit={handleSubmit}>
       <h3>Create new entry</h3>
-      <label>Title </label>
-      <input
-        type="text"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-      />
-      <label>Spend: </label>
-      <input
-        type="number"
-        onChange={(e) => setSpend(e.target.value)}
-        value={spend}
-      />
-      <label>Borrow: </label>
-      <input
-        type="number"
-        onChange={(e) => setBorrow(e.target.value)}
-        value={borrow}
-      />
+      <div className="main">
+      <div className="container">
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          placeholder="Title"
+        />
+      </div>
+      <div className="container">
+        <input
+          type="number"
+          onChange={(e) => setSpend(e.target.value)}
+          value={spend}
+          placeholder="Spend"
+        />
+      </div>
+
+      <div className="container">
+        <input
+          type="number"
+          onChange={(e) => setBorrow(e.target.value)}
+          value={borrow}
+          placeholder="Borrow"
+        />
+      </div>
+      </div>
+      
+      <button className="bn29 btn">Submit</button>
     </form>
   );
 }
